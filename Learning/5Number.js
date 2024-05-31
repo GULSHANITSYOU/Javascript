3bd6fa47fce35446464ebc0842c73ffa594844cb
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-05-31 16:56:09 +0530

    Maths object

---
+++ Learning/5Number.js
@@ -0,0 +1,41 @@
+
+/************** Numbers **************/
+const score = 100; 
+
+console.log(score.toString());
+
+const balance = new Number(700);  // using constructor function 
+
+console.log(balance.valueOf() , "\n\n\n");
+
+/***** Methods of Number object ********/
+
+/*
+
+ toString 
+ value of 
+ .
+ .
+ ... etc 
+
+
+*/
+
+
+
+
+
+// +++++++++++++++ Maths ++++++++++++++++++++ 
+
+console. log (Math) ;
+console. log (Math. abs(-4) ) ;
+console. log (Math. round (4.6) ) ;
+console. log (Math. ceil (4.2));
+console. log (Math. floor (4.9) ) ;
+
+console.log(Math.max(4,1,2,5,7,9));
+console.log(Math.min(4,1,2,5,7,9));
+console.log(Math.random( ) ) ;
+
+
+
