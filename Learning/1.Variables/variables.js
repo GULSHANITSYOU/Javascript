55b2466c46bf607a18eeb46372b3683d34ff5042
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-05-31 07:43:38 +0530

    Learned variables

---
+++ Learning/1.Variables/variables.js
@@ -0,0 +1,32 @@
+
+
+let Name =  "Gulshan"
+var lastName = "Kumar"
+const password = 1276 
+
+{
+    id = "gulshanitsyou"
+}
+
+array = [Name , lastName , password , id , "Ha bhai ye bhi "]; 
+
+console.table([Name , lastName ,password , id]);
+console.table(array);
+
+console.log( id);
+
+/*
+ 
+
+there are  3 ways to declare a variable 
+1 var  (ishu in block scop and function scop )
+2 let 
+3 const 
+
+by default  
+
+id = "gulshanitsyou" (var); 
+
+
+
+*/
