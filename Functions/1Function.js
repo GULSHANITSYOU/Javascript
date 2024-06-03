e74e873f084ee534af1c43de69d0c454ef3d6ceb
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-06-03 11:23:29 +0530

    Colore changing project

--- Functions/1Function.js
+++ Functions/1Function.js
@@ -6,14 +6,17 @@ function calculate ( val1 , val2 , ...num){ // ... rest opertor (bunduls the all
 }
 
 
-calculate(2,5,4,7,8,6,9,7); 
+// calculate(2,5,4,7,8,6,9,7); 
 
 
 //+++++++++++++++ Functions with object ++++++++++++++++
 
 const item = {
     itmeName : "T-shirt",
-    prices: "999"
+    prices: "999",
+    ProductDetails: function(){
+        console.log(`Item is ${this.itmeName} and the price is ${this.prices}`);  //+++++++++ This Keyword ++++++++
+    }
 }
 
 
@@ -23,4 +26,39 @@ function showItem(anyobject){
 
 
 showItem(item); 
+
+
+
+
+function one(){
+
+    const name = "gulshan"; 
+
+    function two(){
+        const lastName = "Kumar";
+
+        function three(){
+            const midName = "Kuch nhi "; 
+            console.log(name  + lastName + midName );
+        }
+
+        three();
+    }
+
+    two(); 
+}
+
+
+// one(); 
+
+
+console.log(a , b); 
+
+let a = 0 ; 
+let b = 0 ; 
+var cbd = 9  ;
+
+
+
+
 
