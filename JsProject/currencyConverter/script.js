465b49b3aaeda9e94034f5431bac3dbd821d8954
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-06-12 23:29:17 +0530

    Add currency data function and targets

--- JsProject/currencyConverter/script.js
+++ JsProject/currencyConverter/script.js
@@ -7,6 +7,18 @@ function currencyData(currency){
   
 }
 
+// +++++++++++++++ Targets ++++++++++++++++++++++
+const choosecurrencyfrom = document.querySelector('#from-currency');
+const choosecurrencyTo = document.querySelector('#to-currency');
+const fromAmountInput = document.querySelector('#from-currency');
+const ToAmountInput = document.querySelector('#to-currency');
+const swapbtn = document.querySelector('#swap-btn');
+
+
+
+
+
+
 
 
 
