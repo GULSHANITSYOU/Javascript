8c359c14ce0594e22faefc0496caa259313b4e1c
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-07-24 21:41:52 +0530

    Node

---
+++ Server/Index.js
@@ -0,0 +1,11 @@
+
+
+const http = require("http"); 
+
+const myserver = http.createServer((req,res)=>{
+    console.log(req.headers);
+    res.end("<h1>Hello from server! </h1>");
+});
+
+myserver.listen(5001,()=> console.log("Server started !")); 
+
