b0e70fbc8a4cdd379d922685d6766557245bcac2
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-08-02 16:20:13 +0530

    No Code

--- JsProject/URL shortner/index.js
+++ JsProject/URL shortner/index.js
@@ -29,7 +29,7 @@ app.set("view engine", "ejs");
 app.set("veiws", path.resolve("./views"));
 
 // Routes
-app.use("/", checkAuth , StaticRoute);
+app.use("/", checkAuth ,  StaticRoute);
 app.use("/url",restrictToLoggedinOnly, UrlRoute);
 app.use("/user",UserRoute); 
 
