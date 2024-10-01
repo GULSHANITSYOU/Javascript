7ec608f32fad8c6550baf6f9a98f6315002948ab
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-10-01 18:40:45 +0530

    Varibles

---
+++ Typescript/2.variables.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello my name is Gulshna!";
+var num = 10;
+num.toFixed();
+var b = true;
+console.log(greetings);
+num = "hello"; // error
+console.log(num)
